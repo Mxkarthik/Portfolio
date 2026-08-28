a8f646cf3a4f94a5c9cba2a85448626d2831857a
MxKarthik <maxwhitie1234@gmail.com>
2026-08-28 22:47:25 +0530

    Imported all the assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
+/**
+ * Node Modules 
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+/**
+ * Components
+ */
+import App from '@/App'
+
+/**
+ * Assets
+ */
+import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
